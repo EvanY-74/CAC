546959c7a1d7ddf333bba46cf39952c7d9e31a71
EvanY-74 <138799210+EvanY-74@users.noreply.github.com>
2025-07-25 17:16:05 -0400

    I think we are almost good now

---
+++ README.md
@@ -0,0 +1,7 @@
+
+# HOW TO START DEVELOPING
+1. Clone GitHub repository (if you haven't done so already)
+2. Run: "python -m venv venv". This will create a virtual environment
+3. Activate it: "venv\Scripts\activate"
+4. Install all the dependencies: "pip install ."
+5. Run the file: "python main.py"
--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\aryan\web dev projects\CAC\VoteSmart\VoteSmart\venv
+set VIRTUAL_ENV=C:\Users\587user\OneDrive - Wake County Public Schools\Programs\CAC\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
