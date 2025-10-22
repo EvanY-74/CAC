a79de808efa54c7f68eb467953bd0487caba30d1
EvanY-74 <138799210+EvanY-74@users.noreply.github.com>
2025-10-22 19:20:03 -0400

    Refactored data to be in separate files
    There is no longer a data.json but candidates.json, glossary.json, etc.

--- data.py
+++ data.py
@@ -1,16 +1,16 @@
 import json
+import os
 
-DATA_PATH = 'data.json'
 
-def get_data():
+def read_file(json_file):
     try:
-        with open(DATA_PATH, 'r') as file:
+        with open(os.path.join('data', json_file + '.json'), 'r') as file:
             return json.load(file)
 
     except FileNotFoundError:
-        print(f'Error: The file "{DATA_PATH}" was not found.')
+        print(f'Error: The file "{json_file}" was not found.')
     except json.JSONDecodeError:
-        print(f'Error: Could not decode JSON from "{DATA_PATH}". Check for valid JSON format.')
+        print(f'Error: Could not decode JSON from "{json_file}". Check for valid JSON format.')
 
 
 def get_all_candidates(attributes = None):
@@ -20,18 +20,18 @@ def get_all_candidates(attributes = None):
     if attributes is None, every attribute will be returned
     """
     if attributes is None:
-        return get_data()['candidates']
+        return read_file('candidates')
 
     if type(attributes) is not list:
         attributes = [attributes]
 
-    return [{key: candidate[key] for key in attributes if key in candidate} for candidate in get_data()['candidates']]
+    return [{key: candidate[key] for key in attributes if key in candidate} for candidate in read_file('candidates')]
 
 
 def search_candidates(attribute, value, unique=True, case_sensitive=False):
     """Search candidates by attribute (most often id or name)"""
 
-    candidates = get_data()['candidates']
+    candidates = read_file('candidates')
 
     if not case_sensitive and type(value) is str:
         value = value.lower()
@@ -47,3 +47,4 @@ def search_candidates(attribute, value, unique=True, case_sensitive=False):
                 results.append(candidate)
 
     return results
+
